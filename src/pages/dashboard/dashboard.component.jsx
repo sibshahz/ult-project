387e4c0902b0a3365d31cb16be3189684b206dac
Shahid Gillani <shahid5ssg@gmail.com>
2022-08-13 16:45:28 +0500

    Added lazy loading and suspense for dashboard projects directory

--- src/pages/dashboard/dashboard.component.jsx
+++ src/pages/dashboard/dashboard.component.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import { Loader } from "@mantine/core";
+import React,{lazy,Suspense} from "react";
 import ProjectForm from "../../components/project-form/project-form.component";
-import ProjectsDirectory from "../../components/projects-directory/projects-directory.component";
+
+const ProjectsDirectory = lazy(() => import('../../components/projects-directory/projects-directory.component'));
 class DashboardPage extends React.Component {
   
   render(){
     return (
       <>
         {/* <ProjectsDirectory /> */}
-       <ProjectsDirectory />
+        <Suspense fallback={<Loader sx={{ position:'absolute',width:"10%",height:"10%",top:"45%",left:"45%" }} />}>
+          <ProjectsDirectory />
+        </Suspense>
+
         
       </>
      );
